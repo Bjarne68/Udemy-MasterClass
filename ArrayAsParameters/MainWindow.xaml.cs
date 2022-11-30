a64f800affde663776d2f25f25bd62679786f226
Bjarne68 <bjan2100@student.miun.se>
2022-11-30 11:11:01 -0300

    Lektion 95

---
+++ ArrayAsParameters/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ArrayAsParameters
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int[] happyness = new int[] { 1, 89, 17, 99, 6};
+            GetHappyness(happyness);
+
+            foreach (int grade in happyness)
+            {
+                MessageBox.Show($"{grade}");
+            }
+
+           
+
+            static void GetHappyness(int[] happyness)
+            {
+                int size= happyness.Length;
+                
+                for (int i = 0; i < size; i++)
+                {
+                    happyness[i] = happyness[i] +2;
+                }
+              
+               
+            }
+        }
+    }
+}
---
+++ ParamsExample/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParamsExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int price = 50;
+            float pi = 3.14f;
+            char at = '@';
+            string book = "The Hobbit";
+
+            ParamsMetod();
+            ParamsMethod2(price, pi, at, book);
+            ParamsMethod2("Hello", 5.3, '$');
+
+        }
+
+        private static void ParamsMetod(params string[] sentence)
+        {
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                MessageBox.Show(sentence[i]);
+            }
+        }
+
+        private static void ParamsMethod2(params object[] stuff)
+        {
+            foreach (object obj in stuff)
+            {
+                Console.WriteLine(obj);
+               
+            }
+        }
+    }
+}
